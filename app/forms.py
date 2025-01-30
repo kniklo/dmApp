c76d64c9ef7a8392890845da2356258a8ef96727
kniklo <kateryna.stepanova@nure.ua>
2025-01-30 13:39:41 +0300

    last commit on this project

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.fields.simple import TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+
+from wtforms.fields import FloatField
 import sqlalchemy as sa
 from app import db
 from app.models import User
@@ -35,7 +36,32 @@ class RegistrationForm(FlaskForm):
       raise ValidationError('Please use a different email address.')
 
 
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = db.session.scalar(sa.select(User).where(
+                User.username == username.data))
+            if user is not None:
+                raise ValidationError('Please use a different username.')
+
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
+
+
 class PostForm(FlaskForm):
     post = TextAreaField('Say something', validators=[
         DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
+
+
+class BaseAlternativeForm(FlaskForm):
+    submit = SubmitField('Save')
